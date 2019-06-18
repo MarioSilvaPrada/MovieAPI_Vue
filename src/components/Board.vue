7ee292039573f8c0690142d202724385d8e34353
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-06-18 22:50:29 +0100

    adding

---
+++ src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="board">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .board {
+    width: 60rem;
+    box-shadow: 0 2rem 6rem rgba(0, 0, 0, 0.3);
+    height: 40rem;
+    border-radius: 1.5rem;
+    background: #171934;
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    margin: 6rem 0;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <Board/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Board from "@/components/Board.vue";
+
+export default {
+  name: "home",
+  components: {
+    Board
+  }
+};
+</script>
+
+<style >
+@import "../config/style.js";
+
+.container {
+  background: #7f7fd5; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #91eae4,
+    #86a8e7,
+    #7f7fd5
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #91eae4,
+    #86a8e7,
+    #7f7fd5
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
